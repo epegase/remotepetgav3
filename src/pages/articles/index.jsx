e17dad9e7da22477fb579ba7f7271a5cfa80090f
Duverger PETGA <epegase@gmail.com>
2023-03-16 09:01:26 +0000

    update of articles page

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -21,7 +21,7 @@ function Article({ article }) {
           {formatDate(article.date)}
         </Card.Eyebrow>
         <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
+        <Card.Cta>Lire la Note</Card.Cta>
       </Card>
       <Card.Eyebrow
         as="time"
@@ -38,15 +38,15 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
+        <title>Articles - Duverger PETGA</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="Toutes mes notes sur l'architecture cloud avec AWS et le développement du secteur de l'intelligence artificelle classées par ordre chronologique."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="J'écris sur le Cloud, les systèmes distribués, l'intelligence artificielle et le Venture Capital"
+        intro="Toutes mes notes sur l'architecture cloud avec AWS et le développement du secteur de l'intelligence artificelle classées par ordre chronologique."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
